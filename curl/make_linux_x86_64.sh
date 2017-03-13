cef797391f6400392c49635bee02ea822ff26ead
Sun Daowen <sundaowen@gmail.com>
2017-03-13 10:36:50 +0800

    curl-7.53.1+linux

--- curl/make_linux_x86_64.sh
+++ curl/make_linux_x86_64.sh
@@ -6,20 +6,20 @@ cd "$rootdir"
 rootdir=`pwd`
 target=linux_x86_64
 prefix=$rootdir/$target
-version=`cat $rootdir/version.txt`
+version=`cat "$rootdir/version.txt"`
 rm -rf "$rootdir/$version"
-mkdir "$rootdir/$version"
+mkdir -p "$rootdir/$version"
 cp -rf "$rootdir/../$version/"* "$rootdir/$version"
 rm -rf "$rootdir/project"
-mkdir "$rootdir/project"
+mkdir -p "$rootdir/project"
 cd "$rootdir/project"
-openssl_version=`cat $rootdir/../openssl_build/openssl/version.txt`
-cmake -DBUILD64=ON -C "$rootdir/CMakeLists.txt" -D_OPENSSL_VERSION="$openssl_version" -DOPENSSL_INCLUDE_DIR="$rootdir/../openssl_build/target/include/$target" -DOPENSSL_CRYPTO_LIBRARY="$rootdir/../openssl_build/target/lib/$target/libcrypto.a" -DOPENSSL_SSL_LIBRARY="$rootdir/../openssl_build/target/lib/$target/libssl.a" -DBUILD_CURL_EXE=OFF -DBUILD_TESTING=OFF -DCURL_STATICLIB=ON -DCMAKE_INSTALL_PREFIX="$prefix" "$rootdir/$version"
+openssl_version=`cat "$rootdir/../openssl_build/openssl/version.txt"`
+cmake -DBUILD64=ON -C "$rootdir/CMakeLists.txt" -D_OPENSSL_VERSION="$openssl_version" -DOPENSSL_INCLUDE_DIR="$rootdir/../openssl_build/target/include/$target" -DOPENSSL_CRYPTO_LIBRARY="$rootdir/../openssl_build/target/lib/$target/libcrypto.a" -DOPENSSL_SSL_LIBRARY="$rootdir/../openssl_build/target/lib/$target/libssl.a" -DBUILD_CURL_EXE=OFF -DBUILD_TESTING=OFF -DCURL_STATICLIB=ON -DCURL_DISABLE_LDAP=ON -DCURL_ZLIB=OFF -DCMAKE_INSTALL_PREFIX="$prefix" "$rootdir/$version"
 cmake "$rootdir/$version"
 cmake --build . --target install --config Release --clean-first
-mkdir "$rootdir/../target/include/$target"
+mkdir -p "$rootdir/../target/include/$target"
 cp -rf "$prefix/include/"* "$rootdir/../target/include/$target"
-mkdir "$rootdir/../target/lib/$target"
+mkdir -p "$rootdir/../target/lib/$target"
 cp -f "$prefix/lib/libcurl.a" "$rootdir/../target/lib/$target"
 cd "$cwdir"
 rm -rf "$rootdir/$version"
