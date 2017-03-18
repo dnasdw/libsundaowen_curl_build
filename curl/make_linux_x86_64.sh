9b0df172b1b6fffe3b7715470b2968be4343a6c3
Sun Daowen <sundaowen@gmail.com>
2017-03-18 11:27:54 +0800

    update cmake

--- curl/make_linux_x86_64.sh
+++ curl/make_linux_x86_64.sh
@@ -15,7 +15,6 @@ mkdir -p "$rootdir/project"
 cd "$rootdir/project"
 openssl_version=`cat "$rootdir/../openssl_build/openssl/version.txt"`
 cmake -DBUILD64=ON -C "$rootdir/CMakeLists.txt" -D_OPENSSL_VERSION="$openssl_version" -DOPENSSL_INCLUDE_DIR="$rootdir/../openssl_build/target/include/$target" -DOPENSSL_CRYPTO_LIBRARY="$rootdir/../openssl_build/target/lib/$target/libcrypto.a" -DOPENSSL_SSL_LIBRARY="$rootdir/../openssl_build/target/lib/$target/libssl.a" -DBUILD_CURL_EXE=OFF -DBUILD_TESTING=OFF -DCURL_STATICLIB=ON -DCURL_DISABLE_LDAP=ON -DCURL_ZLIB=OFF -DCMAKE_INSTALL_PREFIX="$prefix" "$rootdir/$version"
-cmake "$rootdir/$version"
 cmake --build . --target install --config Release --clean-first
 mkdir -p "$rootdir/../target/include/$target"
 cp -rf "$prefix/include/"* "$rootdir/../target/include/$target"
